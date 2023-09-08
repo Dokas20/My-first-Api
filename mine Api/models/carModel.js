ec52801c3f0ebe2436c1ddec5d2c370763c9415b
DuarteSilva2023 <138068182+DuarteSilva2023@users.noreply.github.com>
2023-09-08 12:02:29 +0100

    dia  8 de setenbro

--- mine Api/models/carModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const Car = mongoose.model('Car', {
-    userId: {type: String, require: true},
-    products: {type: Array, require: true},
-})
-module.exports= Car
--- mine Api/routs/userRouts.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-const userControler= require('../controlers/userControlers')
-router.post('/', userControler.create)
-router.get('/', userControler.findAll)
-router.get('/:name', userControler.findOne)
-
-module.exports = router
