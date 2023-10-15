7399a33515980169f14340dc1852fff6ed7ee816
DuarteSilva2023 <138068182+DuarteSilva2023@users.noreply.github.com>
2023-10-15 15:40:42 +0100

    dia  15 de outubro

--- public/users/LoginAndSigIn.js
+++ public/users/LoginAndSigIn.js
@@ -1,32 +1,69 @@
+
 const btnSubmit = document.getElementById('loginUser')
 const id = sessionStorage.getItem('prodId')
 
-btnSubmit.addEventListener('submit',async (e)=> {
-    e.preventDefault()
-    const email = document.getElementById('iemail').value
-    const emailValid = validEmails(email)
-    const pass = document.getElementById('ipass').value
-    const response = document.getElementById('apiResponse')
-    if(emailValid){
+if(btnSubmit){
 
-        const result = await LoginUser(email,pass)
-        if(result){
-           sessionStorage.setItem('token', `${result}`)
-           if(id){
+    btnSubmit.addEventListener('submit',async (e)=> {
+        e.preventDefault()
+        const email = document.getElementById('iemail').value
+        const emailValid = validEmails(email)
+        const pass = document.getElementById('ipass').value
+        const response = document.getElementById('apiResponse')
+
+        if(emailValid){  
+            const result = await LoginUser(email,pass)
+            if(result){
+
+                sessionStorage.setItem('token', `${result}`)
+            if(id){
                window.location = 'http://localhost:3000/users/oneProd.html'
             }else{
-            window.location = 'http://localhost:3000/users/mainPage.html'
-           }
-           
-        } else{
+                window.location = 'http://localhost:3000/users/mainPage.html'
+            }}
+            else {
             response.innerText = 'Usuário não encontrado'
+            }
         }
+        else{
+        response.innerText = 'Email inválido'        
+    }
+})
 
+} else{
+
+    document.getElementById('sigInUser').addEventListener('submit', async (e) => {
+        e.preventDefault()
+        const email = document.getElementById('iemail').value
+        const emailValid = validEmails(email)
+        const pass = document.getElementById('ipass').value
+        const name = document.getElementById('iname').value
+        const response = document.getElementById('apiResponse')
+        if(emailValid){
+        const result = await sigInUser (name, emailValid, pass)
+        if(result.msg){
+            response.innerText = 'Email já utilizado'
+        }
+        else {
+            const result = await LoginUser(email,pass)
+            if(result){
+
+                sessionStorage.setItem('token', `${result}`)
+            if(id){
+               window.location = 'http://localhost:3000/users/oneProd.html'
+            }else{
+                window.location = 'http://localhost:3000/users/mainPage.html'
+            }}
+        }
+       
+       
     }else{
         response.innerText = 'Email inválido'        
     }
-
+    
 })
+}
+
 
 function validEmails(email){
     ///([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
@@ -52,7 +89,7 @@ async function sigInUser(name, email, password) {
         console.log(e.error)
     })
     const siginReturn = await result.json()
-    if (siginReturn) return console.log(siginReturn)
+    return siginReturn
 }
 
 async function LoginUser(email, password) {
